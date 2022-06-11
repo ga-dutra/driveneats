44c01055a6488398d527055c1996d28deaebf72c
Gabriel Dutra <ga.acdutra@gmail.com>
2022-06-10 22:02:07 -0300

    javascript otimizado

--- scripts.js
+++ scripts.js
@@ -1,289 +1,80 @@
-function selecionaItemP1() {
-  let elemento_p1 = document.querySelector(".p1");
-  let elemento_p2 = document.querySelector(".p2").className;
-  let elemento_p3 = document.querySelector(".p3").className;
-  let elemento_p4 = document.querySelector(".p4").className;
-  elemento_p1.classList.toggle("borda_verde");
-  if (elemento_p2[26] === "b") {
-    let elemento_p2 = document.querySelector(".p2");
-    elemento_p2.classList.toggle("borda_verde");
+let contador = 0;
+
+function selecionaPrato(elemento) {
+  // 1. Verifica se o botão já foi clicado. Nesse caso, a borda é removida
+  // 2. Caso não tenha sido clicado, a borda é acionada, e caso outro botão
+  // da mesma categoria esteja selecionado, este é removido.
+  const verificaPrato = document.querySelector(".prato_principal.borda_verde");
+  let a = elemento.classList;
+  // a representa a string que possui as classes do elemento.
+  if (String(a)[39] === "b") {
+    elemento.classList.remove("borda_verde");
+    contador--;
+    fecharPedido();
+  } else {
+    if (verificaPrato !== null) {
+      verificaPrato.classList.remove("borda_verde");
+      contador--;
+    }
+    elemento.classList.add("borda_verde");
+    contador++;
+    fecharPedido();
   }
-  if (elemento_p3[26] === "b") {
-    let elemento_p3 = document.querySelector(".p3");
-    elemento_p3.classList.toggle("borda_verde");
-  }
-  if (elemento_p4[26] === "b") {
-    let elemento_p4 = document.querySelector(".p4");
-    elemento_p4.classList.toggle("borda_verde");
-  }
-  verificaFecharPedido();
 }
 
-function selecionaItemP2() {
-  let elemento_p1 = document.querySelector(".p1").className;
-  let elemento_p2 = document.querySelector(".p2");
-  let elemento_p3 = document.querySelector(".p3").className;
-  let elemento_p4 = document.querySelector(".p4").className;
-  elemento_p2.classList.toggle("borda_verde");
-  if (elemento_p1[26] === "b") {
-    let elemento_p1 = document.querySelector(".p1");
-    elemento_p1.classList.toggle("borda_verde");
+function selecionaBebida(elemento) {
+  const verificaBebida = document.querySelector(".bebida.borda_verde");
+  let a = elemento.classList;
+  if (String(a)[30] === "b") {
+    elemento.classList.remove("borda_verde");
+    contador--;
+    fecharPedido();
+  } else {
+    if (verificaBebida !== null) {
+      verificaBebida.classList.remove("borda_verde");
+      contador--;
+    }
+    elemento.classList.add("borda_verde");
+    contador++;
+    fecharPedido();
   }
-  if (elemento_p3[26] === "b") {
-    let elemento_p3 = document.querySelector(".p3");
-    elemento_p3.classList.toggle("borda_verde");
-  }
-  if (elemento_p4[26] === "b") {
-    let elemento_p4 = document.querySelector(".p4");
-    elemento_p4.classList.toggle("borda_verde");
-  }
-  verificaFecharPedido();
 }
-function selecionaItemP3() {
-  let elemento_p1 = document.querySelector(".p1").className;
-  let elemento_p2 = document.querySelector(".p2").className;
-  let elemento_p3 = document.querySelector(".p3");
-  let elemento_p4 = document.querySelector(".p4").className;
-  elemento_p3.classList.toggle("borda_verde");
-  if (elemento_p1[26] === "b") {
-    let elemento_p1 = document.querySelector(".p1");
-    elemento_p1.classList.toggle("borda_verde");
+function selecionaSobremesa(elemento) {
+  const verificaSobremesa = document.querySelector(".sobremesa.borda_verde");
+  let a = elemento.classList;
+  if (String(a)[33] === "b") {
+    elemento.classList.remove("borda_verde");
+    contador--;
+    fecharPedido();
+  } else {
+    if (verificaSobremesa !== null) {
+      verificaSobremesa.classList.remove("borda_verde");
+      contador--;
+    }
+    elemento.classList.add("borda_verde");
+    contador++;
+    fecharPedido();
   }
-  if (elemento_p2[26] === "b") {
-    let elemento_p2 = document.querySelector(".p2");
-    elemento_p2.classList.toggle("borda_verde");
-  }
-  if (elemento_p4[26] === "b") {
-    let elemento_p4 = document.querySelector(".p4");
-    elemento_p4.classList.toggle("borda_verde");
-  }
-  verificaFecharPedido();
-}
-function selecionaItemP4() {
-  let elemento_p1 = document.querySelector(".p1").className;
-  let elemento_p2 = document.querySelector(".p2").className;
-  let elemento_p3 = document.querySelector(".p3").className;
-  let elemento_p4 = document.querySelector(".p4");
-  elemento_p4.classList.toggle("borda_verde");
-  if (elemento_p1[26] === "b") {
-    let elemento_p1 = document.querySelector(".p1");
-    elemento_p1.classList.toggle("borda_verde");
-  }
-  if (elemento_p2[26] === "b") {
-    let elemento_p2 = document.querySelector(".p2");
-    elemento_p2.classList.toggle("borda_verde");
-  }
-  if (elemento_p3[26] === "b") {
-    let elemento_p3 = document.querySelector(".p3");
-    elemento_p3.classList.toggle("borda_verde");
-  }
-  verificaFecharPedido();
-}
-
-function selecionaItemB1() {
-  let elemento_b1 = document.querySelector(".b1");
-  let elemento_b2 = document.querySelector(".b2").className;
-  let elemento_b3 = document.querySelector(".b3").className;
-  let elemento_b4 = document.querySelector(".b4").className;
-  elemento_b1.classList.toggle("borda_verde");
-  if (elemento_b2[26] === "b") {
-    let elemento_b2 = document.querySelector(".b2");
-    elemento_b2.classList.toggle("borda_verde");
-  }
-  if (elemento_b3[26] === "b") {
-    let elemento_b3 = document.querySelector(".b3");
-    elemento_b3.classList.toggle("borda_verde");
-  }
-  if (elemento_b4[26] === "b") {
-    let elemento_b4 = document.querySelector(".b4");
-    elemento_b4.classList.toggle("borda_verde");
-  }
-  verificaFecharPedido();
-}
-function selecionaItemB2() {
-  let elemento_b1 = document.querySelector(".b1").className;
-  let elemento_b2 = document.querySelector(".b2");
-  let elemento_b3 = document.querySelector(".b3").className;
-  let elemento_b4 = document.querySelector(".b4").className;
-  elemento_b2.classList.toggle("borda_verde");
-  if (elemento_b1[26] === "b") {
-    let elemento_b1 = document.querySelector(".b1");
-    elemento_b1.classList.toggle("borda_verde");
-  }
-  if (elemento_b3[26] === "b") {
-    let elemento_b3 = document.querySelector(".b3");
-    elemento_b3.classList.toggle("borda_verde");
-  }
-  if (elemento_b4[26] === "b") {
-    let elemento_b4 = document.querySelector(".b4");
-    elemento_b4.classList.toggle("borda_verde");
-  }
-  verificaFecharPedido();
-}
-function selecionaItemB3() {
-  let elemento_b1 = document.querySelector(".b1").className;
-  let elemento_b2 = document.querySelector(".b2").className;
-  let elemento_b3 = document.querySelector(".b3");
-  let elemento_b4 = document.querySelector(".b4").className;
-  elemento_b3.classList.toggle("borda_verde");
-  if (elemento_b1[26] === "b") {
-    let elemento_b1 = document.querySelector(".b1");
-    elemento_b1.classList.toggle("borda_verde");
-  }
-  if (elemento_b2[26] === "b") {
-    let elemento_b2 = document.querySelector(".b2");
-    elemento_b2.classList.toggle("borda_verde");
-  }
-  if (elemento_b4[26] === "b") {
-    let elemento_b4 = document.querySelector(".b4");
-    elemento_b4.classList.toggle("borda_verde");
-  }
-  verificaFecharPedido();
-}
-function selecionaItemB4() {
-  let elemento_b1 = document.querySelector(".b1").className;
-  let elemento_b2 = document.querySelector(".b2").className;
-  let elemento_b3 = document.querySelector(".b3").className;
-  let elemento_b4 = document.querySelector(".b4");
-  elemento_b4.classList.toggle("borda_verde");
-  if (elemento_b1[26] === "b") {
-    let elemento_b1 = document.querySelector(".b1");
-    elemento_b1.classList.toggle("borda_verde");
-  }
-  if (elemento_b2[26] === "b") {
-    let elemento_b2 = document.querySelector(".b2");
-    elemento_b2.classList.toggle("borda_verde");
-  }
-  if (elemento_b3[26] === "b") {
-    let elemento_b3 = document.querySelector(".b3");
-    elemento_b3.classList.toggle("borda_verde");
-  }
-  verificaFecharPedido();
-}
-
-function selecionaItemS1() {
-  let elemento_s1 = document.querySelector(".s1");
-  let elemento_s2 = document.querySelector(".s2").className;
-  let elemento_s3 = document.querySelector(".s3").className;
-  let elemento_s4 = document.querySelector(".s4").className;
-  elemento_s1.classList.toggle("borda_verde");
-  if (elemento_s2[26] === "b") {
-    let elemento_s2 = document.querySelector(".s2");
-    elemento_s2.classList.toggle("borda_verde");
-  }
-  if (elemento_s3[26] === "b") {
-    let elemento_s3 = document.querySelector(".s3");
-    elemento_s3.classList.toggle("borda_verde");
-  }
-  if (elemento_s4[26] === "b") {
-    let elemento_s4 = document.querySelector(".s4");
-    elemento_s4.classList.toggle("borda_verde");
-  }
-  verificaFecharPedido();
-}
-
-function selecionaItemS2() {
-  let elemento_s1 = document.querySelector(".s1").className;
-  let elemento_s2 = document.querySelector(".s2");
-  let elemento_s3 = document.querySelector(".s3").className;
-  let elemento_s4 = document.querySelector(".s4").className;
-  elemento_s2.classList.toggle("borda_verde");
-  if (elemento_s1[26] === "b") {
-    let elemento_s1 = document.querySelector(".s1");
-    elemento_s1.classList.toggle("borda_verde");
-  }
-  if (elemento_s3[26] === "b") {
-    let elemento_s3 = document.querySelector(".s3");
-    elemento_s3.classList.toggle("borda_verde");
-  }
-  if (elemento_s4[26] === "b") {
-    let elemento_s4 = document.querySelector(".s4");
-    elemento_s4.classList.toggle("borda_verde");
-  }
-  verificaFecharPedido();
-}
-function selecionaItemS3() {
-  let elemento_s1 = document.querySelector(".s1").className;
-  let elemento_s2 = document.querySelector(".s2").className;
-  let elemento_s3 = document.querySelector(".s3");
-  let elemento_s4 = document.querySelector(".s4").className;
-  elemento_s3.classList.toggle("borda_verde");
-  if (elemento_s1[26] === "b") {
-    let elemento_s1 = document.querySelector(".s1");
-    elemento_s1.classList.toggle("borda_verde");
-  }
-  if (elemento_s2[26] === "b") {
-    let elemento_s2 = document.querySelector(".s2");
-    elemento_s2.classList.toggle("borda_verde");
-  }
-  if (elemento_s4[26] === "b") {
-    let elemento_s4 = document.querySelector(".s4");
-    elemento_s4.classList.toggle("borda_verde");
-  }
-  verificaFecharPedido();
-}
-function selecionaItemS4() {
-  let elemento_s1 = document.querySelector(".s1").className;
-  let elemento_s2 = document.querySelector(".s2").className;
-  let elemento_s3 = document.querySelector(".s3").className;
-  let elemento_s4 = document.querySelector(".s4");
-  elemento_s4.classList.toggle("borda_verde");
-  if (elemento_s1[26] === "b") {
-    let elemento_s1 = document.querySelector(".s1");
-    elemento_s1.classList.toggle("borda_verde");
-  }
-  if (elemento_s2[26] === "b") {
-    let elemento_s2 = document.querySelector(".s2");
-    elemento_s2.classList.toggle("borda_verde");
-  }
-  if (elemento_s3[26] === "b") {
-    let elemento_s3 = document.querySelector(".s3");
-    elemento_s3.classList.toggle("borda_verde");
-  }
-  verificaFecharPedido();
 }
 
 function fecharPedido() {
   let texto_final = document.querySelector(".final span");
-  texto_final.innerHTML = "Fechar pedido";
   let deixa_verde = document.querySelector(".final button");
-  deixa_verde.classList.add("deixa_verde");
+  if (contador === 3) {
+    texto_final.innerHTML = "Fechar pedido";
+    deixa_verde.classList.add("deixa_verde");
+  }
+  if (contador !== 3) {
+    deixa_verde.classList.remove("deixa_verde");
+    texto_final.innerHTML = "Selecione os 3 itens para fechar o pedido";
+  }
 }
 
-function verificaFecharPedido() {
-  let elemento_p1 = document.querySelector(".p1").className;
-  let elemento_p2 = document.querySelector(".p2").className;
-  let elemento_p3 = document.querySelector(".p3").className;
-  let elemento_p4 = document.querySelector(".p4").className;
-  let elemento_b1 = document.querySelector(".b1").className;
-  let elemento_b2 = document.querySelector(".b2").className;
-  let elemento_b3 = document.querySelector(".b3").className;
-  let elemento_b4 = document.querySelector(".b4").className;
-  let elemento_s1 = document.querySelector(".s1").className;
-  let elemento_s2 = document.querySelector(".s2").className;
-  let elemento_s3 = document.querySelector(".s3").className;
-  let elemento_s4 = document.querySelector(".s4").className;
-
-  if (
-    (elemento_p1[26] === "b") |
-    (elemento_p2[26] === "b") |
-    (elemento_p3[26] === "b") |
-    (elemento_p4[26] === "b")
-  ) {
-    if (
-      (elemento_b1[26] === "b") |
-      (elemento_b2[26] === "b") |
-      (elemento_b3[26] === "b") |
-      (elemento_b4[26] === "b")
-    ) {
-      if (
-        (elemento_s1[26] === "b") |
-        (elemento_s2[26] === "b") |
-        (elemento_s3[26] === "b") |
-        (elemento_s4[26] === "b")
-      ) {
-        fecharPedido();
-      }
-    }
+function enviaPedido() {
+  if (contador !== 3) {
+    alert("Por favor, selecione 1 prato, 1 bebida e 1 sobremesa");
+  }
+  if (contador === 3) {
+    alert("Vamos enviar o pedido ao restaurante!");
   }
 }
